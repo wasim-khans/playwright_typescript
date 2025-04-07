5b9675324500c45a80c5cdf8d418d731ca5eb67b
Wasim <58463007+x-Waseem-x@users.noreply.github.com>
2025-04-07 21:29:19 +0500

    Minor refactor of LoginState cases

--- e2eWithPOM/tests/e2eWithDifferentUsersUsingLoginState.spec.js
+++ e2eWithPOM/tests/e2eWithDifferentUsersUsingLoginState.spec.js
@@ -2,25 +2,22 @@ const { test, expect } = require('@playwright/test');
 const { POManager } = require('../pageobjects/POManager.js');
 const testData = require('../testdata/e2eData.json');
 const { LoginAndTokenManagerUsingLoginState } = require('../authentication/LoginAndTokenManagerUsingLoginState.js');
-let loginAndTokenManagerUsingLoginState;
-let poManager;
 
+let poManager;
 let managerSessionFilePath;
 let adminSessionFilePath;
-let adminContext;
-let managerContext;
-let adminPage;
+
 test.beforeAll(async ({ browser }) => {
-    adminContext = await browser.newContext();
-    managerContext = await browser.newContext();
-    adminPage = await adminContext.newPage();
+
+    const adminContext = await browser.newContext();
+    const managerContext = await browser.newContext();
+    const adminPage = await adminContext.newPage();
     const managerPage = await managerContext.newPage();
-    loginAndTokenManagerUsingLoginState = new LoginAndTokenManagerUsingLoginState(adminPage,managerPage);
+    const loginAndTokenManagerUsingLoginState = new LoginAndTokenManagerUsingLoginState(adminPage,managerPage);
     adminSessionFilePath = await loginAndTokenManagerUsingLoginState.loginWithAdminRoleAndSaveStateAndReturnPath();
     await adminContext.close();
     managerSessionFilePath = await loginAndTokenManagerUsingLoginState.loginWithManagerRoleAndSaveStateAndReturnPath();
     await managerContext.close();
-    
 
 })
 
@@ -29,12 +26,11 @@ test('Login with new broswerContext loading the state from saved adminSessionfil
 
     const adminContext = await browser.newContext({ storageState: adminSessionFilePath })
     const page = await adminContext.newPage();
-    const poManager = new POManager(page)
+    poManager = new POManager(page)
     await page.pause()
     const dashboardPage = await poManager.getDashboardPage();
     await dashboardPage.navigateFirstTimeUsingTokenSetup();
     await page.pause()
-
 
 })
 
@@ -42,31 +38,24 @@ test('Login with new broswerContext loading the state from saved managerSessionF
 
     const managerContext = await browser.newContext({ storageState: managerSessionFilePath })
     const page = await managerContext.newPage();
-    const poManager = new POManager(page)
-    
+    poManager = new POManager(page)
     const dashboardPage = await poManager.getDashboardPage();
     await dashboardPage.navigateFirstTimeUsingTokenSetup();
-    
-
 })
 
 test.only('PlayAround with locators and assertions', async ({ browser }) => {
 
     const managerContext = await browser.newContext({ storageState: managerSessionFilePath })
     const page = await managerContext.newPage();
-    const poManager = new POManager(page)
+    poManager = new POManager(page)
     const dashboardPage = await poManager.getDashboardPage();
     await dashboardPage.navigateFirstTimeUsingTokenSetup();
-    await page.pause()
-
-    // expect(await page.getByText('User can only see maximum 9 products on a page')).toBeVisible();
     let allProducts=await page.locator(".card-body")
     let zaraCoatTile= allProducts.filter({hasText:"ZARA COAT 3"})
     let priceElement=await zaraCoatTile.getByText('$')
     let priceElementText=await priceElement.textContent();
     console.log('Zara price element value = ',priceElementText)
-    let actualPriceValue=parseFloat(priceElementText.replace('$',''))
+    let actualPriceValue=parseFloat(priceElementText.replace('$','').trim())
     expect(actualPriceValue).toBeGreaterThan(0)
     
-
 })
